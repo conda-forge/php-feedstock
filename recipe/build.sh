b9d679ffa2a56e7c4985769ff3a8e000a81e1f11
fhoehle <fhoehle@users.noreply.github.com>
2021-08-02 07:43:38 +0200

    skip failing test ext/standard/tests/network/bug73594.phpt

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,7 @@ set -ex
 
 # remove test failing in non interactive shell
 rm ext/standard/tests/file/lstat_stat_variation10.phpt
+rm ext/standard/tests/network/bug73594.phpt
 
 ./configure --prefix=$PREFIX --with-iconv=$PREFIX --with-openssl=$PREFIX --with-libxml-dir=$PREFIX
 make -j${CPU_COUNT}
