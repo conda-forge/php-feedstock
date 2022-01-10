8e2d7405bca03cb060f27e3ab1840d1491304488
Wolf Vollprecht <w.vollprecht@gmail.com>
2022-01-10 18:38:50 +0100

    ..

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,12 @@ set -ex
 rm ext/standard/tests/file/lstat_stat_variation10.phpt
 rm ext/standard/tests/network/bug73594.phpt
 
-./configure --prefix=$PREFIX --with-iconv=$PREFIX --with-openssl=$PREFIX --with-libxml-dir=$PREFIX
+./configure --prefix=$PREFIX \
+            --with-iconv=$PREFIX \
+            --with-openssl=$PREFIX \
+            --with-libxml-dir=$PREFIX \
+            --with-external-pcre
+
 make -j${CPU_COUNT}
 
 export NO_INTERACTION=1
