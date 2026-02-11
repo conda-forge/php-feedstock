bce9bbed28f7d51b6ba86c532ab26043a28143d4
UENO, M. <eunosium-1128@live.jp>
2026-02-12 01:29:42 +0900

    Set OPCACHE_FLAG for Linux and OSX platforms
    Add conditional OPCACHE_FLAG based on target platform.

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,11 +6,18 @@ set -exo pipefail
 rm ext/standard/tests/file/lstat_stat_variation10.phpt
 rm ext/standard/tests/network/bug73594.phpt
 
+if [[ "${target_platform}" == "linux-"* ]]; then
+    OPCACHE_FLAG="--enable-opcache"
+elif [[ "${target_platform}" == "osx-"* ]]; then
+    OPCACHE_FLAG="--disable-opcache"
+fi
+
 ./configure --prefix=${PREFIX} \
             --with-iconv=${PREFIX} \
             --with-openssl=${PREFIX} \
             --with-libxml-dir=${PREFIX} \
-            --with-external-pcre
+            --with-external-pcre \
+            OPCACHE_FLAG
 
 make -j${CPU_COUNT}
 
