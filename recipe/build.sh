0a406d62bc25004751f291007ae44ed2f3165cad
Wolf Vollprecht <w.vollprecht@gmail.com>
2026-03-18 21:11:35 +0100

    update to latest php release

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,13 @@
 
 set -ex
 
+export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH
+
+# PHP's configure uses aarch64, but conda sets arm64 on macOS
+if [[ "$(uname -s)" == "Darwin" && "$(uname -m)" == "arm64" ]]; then
+    export PHP_BUILD_HOST="aarch64-apple-darwin$(uname -r)"
+fi
+
 # remove test failing in non interactive shell
 rm ext/standard/tests/file/lstat_stat_variation10.phpt
 rm ext/standard/tests/network/bug73594.phpt
@@ -13,10 +20,33 @@ if [[ "$CI" == "travis" ]]; then
 fi
 
 ./configure --prefix=$PREFIX \
+            ${PHP_BUILD_HOST:+--build=$PHP_BUILD_HOST --host=$PHP_BUILD_HOST} \
             --with-iconv=$PREFIX \
             --with-openssl=$PREFIX \
-            --with-libxml-dir=$PREFIX \
-            --with-external-pcre
+            --with-external-pcre2=$PREFIX \
+            --with-bz2=$PREFIX \
+            --with-curl=$PREFIX \
+            --with-gmp=$PREFIX \
+            --with-sodium=$PREFIX \
+            --with-zip \
+            --with-zlib \
+            --with-readline=$PREFIX \
+            --with-xsl=$PREFIX \
+            --with-pgsql=$PREFIX \
+            --with-pdo-pgsql=$PREFIX \
+            --enable-intl \
+            --enable-bcmath \
+            --enable-calendar \
+            --enable-exif \
+            --enable-ftp \
+            --enable-mbstring \
+            --enable-pcntl \
+            --enable-shmop \
+            --enable-soap \
+            --enable-sockets \
+            --enable-sysvmsg \
+            --enable-sysvsem \
+            --enable-sysvshm
 
 make -j${CPU_COUNT}
 
