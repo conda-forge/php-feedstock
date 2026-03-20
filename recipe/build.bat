6407ff485273fb6f60bbff00105b0e166cb06bb0
Wolf Vollprecht <w.vollprecht@gmail.com>
2026-03-20 09:23:42 +0100

    ..

--- recipe/build.bat
+++ recipe/build.bat
@@ -14,6 +14,14 @@ sed -i "s/libxml2_a_dll.lib;libxml2_a.lib/libxml2_a_dll.lib;libxml2_a.lib;libxml
 sed -i "s/LIBXML_STATIC \/D LIBXML_STATIC_FOR_DLL \/D //" ext\libxml\config.w32
 sed -i "/ADD_DEF_FILE.*php_libxml2.def/d" ext\libxml\config.w32
 
+rem Guard xmlDllMain call - only needed for static libxml2
+sed -i "s/ifdef HAVE_LIBXML/ifdef LIBXML_STATIC_FOR_DLL/" win32\dllmain.c
+
+rem Fix xsl: add shared lib names and remove LIBXML_STATIC flag
+sed -i "s/libxslt_a.lib/libxslt_a.lib;xslt.lib;libxslt.lib/" ext\xsl\config.w32
+sed -i "s/libexslt_a.lib/libexslt_a.lib;exslt.lib;libexslt.lib/" ext\xsl\config.w32
+sed -i "s/\/D LIBXML_STATIC\"/\"/" ext\xsl\config.w32
+
 call buildconf.bat --force
 if errorlevel 1 exit /b 1
 
